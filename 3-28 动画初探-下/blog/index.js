106ad0e00656dcf1676aa20091016e141aba7ff0
zhanghaojincheng <714066825@qq.com>
2018-03-28 22:30:46 +0800

    3-28动画初探 下

---
+++ 3-28 动画初探-下/blog/index.js
@@ -0,0 +1,22 @@
+
+
+
+
+    $(function() {
+
+
+        $('#btn').click(function() {
+            $('#box').animate({
+                attr: 'y',
+                alter: 200,
+                step: 7,
+                start: 100
+            })
+        })
+
+
+    })
+
+
+
+
