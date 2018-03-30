6f9a496104feda3e38ad93a8efc3575025fd8e7c
zhanghaojincheng <714066825@qq.com>
2018-03-30 22:24:33 +0800

    3-30 透明度渐变

---
+++ 3-30 透明度渐变/blog/index.js
@@ -0,0 +1,21 @@
+
+
+
+
+    $(function() {
+
+
+        $('#btn').click(function() {
+            $('#box').animate({
+                attr: 'y',
+                target: 500,
+                step: 7
+            })
+        })
+
+
+    })
+
+
+
+
